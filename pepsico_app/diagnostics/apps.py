164fdbb130d6ce06a64bbc06ae57a9b8bcc40151
Tomás Aguilera <t.aguilera@duocuc.cl>
2025-11-17 05:22:35 -0300

    traslado app a evidencias

--- pepsico_app/diagnostics/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DiagnosticsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'diagnostics'
--- pepsico_app/login/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(label="Usuario", max_length=30,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Usuario'}))
-    password = forms.CharField(label="Contraseña",
-                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña'}))
